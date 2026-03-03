85e3cbbdf6ba4af084d7376f566c13cf81d39ab7
Hemith Kumar J <hemithj@gmail.com>
2026-03-04 01:31:50 +0530

    Installed @vercel/analytics dependency and implemented in App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FSDIntro from './pages/FSDIntro';
 import PageNotFound from './pages/PageNotFound';
 
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const location = useLocation();
@@ -25,6 +26,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
         <SpeedInsights />
+        <Analytics />
       </main>
     </>
   );
